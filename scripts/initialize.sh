e95dfb03220a2b2d591f963f06ad1ed078f94512
Kavindu Athaudha <kavindu@kih.com.sg>
2023-11-24 23:39:33 +1300

    Migrated the container format to v2.
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 # Setup dependencies
-apt install -y npm
+##  Install NodeJS
+NODE_MAJOR=20
+sudo apt install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+apt update
+apt install -y nodejs
+
+##  Install packages
 npm i --omit dev
 npm run build
